44a146077d2e576b8967483449d983b928ea5003
Autojadame <autojadame@gmail.com>
2023-08-16 16:30:17 +0200

    vb

---
+++ config-overrides.js
@@ -0,0 +1,37 @@
+const webpack = require("webpack");
+
+module.exports = function override(config) {
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    crypto: false,
+    stream: false,
+    assert: false,
+    http: false,
+    https: false,
+    path:require.resolve("path-browserify"),
+    os: false,
+    url: false,
+    zlib: false
+  });
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.DefinePlugin({
+      'process.env.RANDOM_IV': JSON.stringify(process.env.RANDOM_IV),
+      'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID),
+    })
+  ]);
+  config.ignoreWarnings = [/Failed to parse source map/];
+  config.module.rules.push({
+    test: /\.(js|mjs|jsx)$/,
+    enforce: "pre",
+    loader: require.resolve("source-map-loader"),
+    resolve: {
+      fullySpecified: false,
+    },
+  });
+  return config;
+};
