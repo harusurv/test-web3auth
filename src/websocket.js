9ff24e46a4eb2e426d0835ccc5e2b14f86b811b3
Autojadame <autojadame@gmail.com>
2023-11-28 11:51:48 +0100

    d

--- src/websocket.js
+++ src/websocket.js
@@ -2,7 +2,7 @@
 
 console.log("START WEBSOCKET")
 var WebSocketServer = require('ws').Server,
-wss = new WebSocketServer({port: WEBSOCKET_PORT})
+wss = new WebSocketServer({port: 40510})
 
 wss.on('connection', function (ws) {
   console.log("connection")
