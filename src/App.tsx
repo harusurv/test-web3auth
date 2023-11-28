6f42bce278ec6d3dce6cdc8609fb3cab536f60c7
Autojadame <autojadame@gmail.com>
2023-11-28 13:08:49 +0100

    d

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { getRedirectResult } from "firebase/auth";
 import {auth} from './firebase.js'
 import {encryptKey} from './utils.js'
-import {loginWithGoogle,loginWithApple,loginWithTwitter,loginWithFacebook} from './providers'
 import "./App.css";
 
 
@@ -14,29 +13,15 @@ function App() {
     const init = async () => {
       try {
         const urlParams = new URLSearchParams(window.location.search);
-        const provider = urlParams.get('provider') as string
         const key = urlParams.get('key') as string
         const loginRes = await getRedirectResult(auth)
-        if(key && provider && loginRes === null){
-          localStorage.setItem('key',key);
-          if(provider === "google")
-            loginWithGoogle()
-          else if(provider === "facebook")
-            loginWithFacebook()
-          else if(provider === "twitter")
-            loginWithTwitter()
-          else if(provider === "apple")
-            loginWithApple()
-        }
-        else{
-          const idToken = (await loginRes?.user?.getIdToken(true)) as string;
-          const secretKey = localStorage.getItem("key") as string;
-          localStorage.removeItem("key");
-          const url = "infinity://?type=auth&hash="+encryptKey(idToken,secretKey)
-          setUrlGo(url)
-          window.open(url)
-          setLoggedIn(true)
-        }
+        const idToken = (await loginRes?.user?.getIdToken(true)) as string;
+        const secretKey = localStorage.getItem("key") as string;
+        localStorage.removeItem("key");
+        const url = "infinity://?type=auth&hash="+encryptKey(idToken,secretKey)
+        setUrlGo(url)
+        window.open(url)
+        setLoggedIn(true)
       } catch (error) {
         console.error(error);
       }
