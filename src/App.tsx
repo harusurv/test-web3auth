9c7919d858e5ac845c430b178cb48321c462a408
Autojadame <autojadame@gmail.com>
2023-11-28 13:38:14 +0100

    d

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getRedirectResult } from "firebase/auth";
 import {auth} from './firebase.js'
 import {encryptKey} from './utils.js'
+import {loginWithGoogle,loginWithApple,loginWithTwitter,loginWithFacebook} from './providers'
 import "./App.css";
 
 
@@ -13,11 +14,24 @@ function App() {
     const init = async () => {
       try {
         const urlParams = new URLSearchParams(window.location.search);
-        const secretKey = urlParams.get('key') as string
+        const provider = urlParams.get('provider') as string
+        const key = urlParams.get('key') as string
         const loginRes = await getRedirectResult(auth)
-        console.log(loginRes)
-        if(secretKey && loginRes){
+        if(key && provider && loginRes === null){
+          localStorage.setItem('key',key);
+          if(provider === "google")
+            loginWithGoogle()
+          else if(provider === "facebook")
+            loginWithFacebook()
+          else if(provider === "twitter")
+            loginWithTwitter()
+          else if(provider === "apple")
+            loginWithApple()
+        }
+        else{
           const idToken = (await loginRes?.user?.getIdToken(true)) as string;
+          const secretKey = localStorage.getItem("key") as string;
+          localStorage.removeItem("key");
           const url = "infinity://?type=auth&hash="+encryptKey(idToken,secretKey)
           setUrlGo(url)
           window.open(url)
