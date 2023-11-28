4dc0d53bc4429ecbfb28198a14376fcecdfe30c2
Autojadame <autojadame@gmail.com>
2023-11-28 11:56:56 +0100

    d

--- src/websocket.js
+++ src/websocket.js
@@ -1,8 +1,6 @@
 //import {WEBSOCKET_PORT} from './config.js'
-
-console.log("START WEBSOCKET")
-var WebSocketServer = require('ws').Server,
-wss = new WebSocketServer({port: 40510})
+import ws from 'ws';
+const wss = new ws.Server({port: 40510})
 
 wss.on('connection', function (ws) {
   console.log("connection")
