276f5ff5e68bd72ba99fe668403dd63ea8d12eca
Autojadame <autojadame@gmail.com>
2023-12-11 22:25:46 +0100

    d

---
+++ server_auth/index.js
@@ -0,0 +1,60 @@
+// Setup express
+var express = require('express');
+var session = require('express-session')
+const SocialLoginClass = require('./social_login.js')
+var app = express();
+app.use(session({secret:"sadasdsadniancatasdasdasdadasda"}));
+
+app.listen(5000, () => {
+  console.log(`Example app listening on port ${5000}`)
+})
+// Init
+var socialLogin = new SocialLoginClass({
+	app: app,    					// ExpressJS instance
+  onAuth: function(req, type, accessToken, refreshToken,  params, idToken, done) {
+        const token_id = params.id_token
+        console.log(req)
+        done(null,idToken)
+	}
+});
+
+socialLogin.use({
+	google:	{
+		settings:	{
+		  clientID:"1028970182942-9a379po6lqvbdmj17nhm5ud78qf3ss3a.apps.googleusercontent.com",
+			clientSecret:"c_vW_t551quwco1GKihk1wUG",
+			authParameters:	{
+				scope: 'profile'
+			}
+		}, // Google doesn't take any API key or API secret
+		url:	{
+			auth:		"/auth/google",
+			callback: 	"/auth/google/callback",
+			success:	'/',
+			fail:		'/auth/google/fail'
+		}
+	},
+  twitter:	{
+    settings:	{
+      clientID: 		"aDJ2Mk5VTE9oSU5xY0c3TWJuaXM6MTpjaQ",
+      clientSecret: 	"suulxkcQN1Tm9r22AeX1Uxg5YB2xIEe6aYtRetQ_gxgBJXOfFD"
+    },
+    url:	{
+      auth:		"/auth/twitter",
+      callback: 	"/auth/twitter/callback",
+      success:	'/',
+      fail:		'/auth/twitter/fail'
+    }
+  },
+  apple: {
+    settings:	{
+      clientID: 		"9FXC62Q3S9"
+    },
+    url:	{
+      auth:		"/auth/twitter",
+      callback: 	"/auth/twitter/callback",
+      success:	'/',
+      fail:		'/auth/twitter/fail'
+    }
+  }
+});
---
+++ server_auth/social_login.js
@@ -0,0 +1,124 @@
+var passport 			= require('passport');
+var GoogleStrategy 		= require('passport-google-oauth20').Strategy;
+var TwitterStrategy 	= require('passport-twitter-oauth2.0').Strategy;
+const path = require('path')
+var AppleStrategy 	= require('passport-appleid').Strategy;
+var socialLoginClass = function(options) {
+	var scope 		= this;
+	this.app 		= options.app 		|| {};
+	this.onAuth 	= options.onAuth 	|| function() {};
+	this.url		= options.url		|| 'https://infinitysocial.ddns.net:3003';
+	this.logout		= options.logout	|| {url: '/logout', after:	'/'};
+	this.map		= {
+		apple:		AppleStrategy,
+		twitter:		TwitterStrategy,
+		google:			GoogleStrategy
+	};
+	this.specialCases = {
+		twitter:{
+			setup:	{
+				clientType: "public",
+				state: true,
+				pkce: true,
+				skipExtendedProfile: true
+			}
+		},
+		apple:{
+			setup:	{
+				teamId: "TBVHBX9PYX",
+				privateKeyPath:: path.join(__dirname, "./AuthKey_9FXC62Q3S9.p8"),
+				keyIdentifier: '9FXC62Q3S9'
+			}
+		}
+	}
+
+}
+socialLoginClass.prototype.use = function(settings) {
+	this.settings = settings;
+	this.init();
+}
+socialLoginClass.prototype.init = function() {
+	var scope = this;
+
+	// Setup PassportJS
+	this.app.use(passport.initialize());
+	this.app.use(passport.session());
+	passport.serializeUser(function(user, done) {
+	  done(null, user);
+	});
+	passport.deserializeUser(function(user, done) {
+	  done(null, user);
+	});
+
+	this.app.get(this.logout.url, function(req, res){
+		res.clearCookie('session_key');
+		req.logout();
+		res.redirect(scope.logout.after);
+	});
+
+	var type;
+	for (type in this.settings) {
+		this.setup(type, this.settings[type]);
+	}
+
+
+	// Setup the cache
+	var caching = function(ttl) {
+		this.ttl 	= ttl;
+		this.cache 	= {};
+		var scope = this;
+		setInterval(function() {
+			var i;
+			var t = new Date().getTime();
+			for (i in scope.cache) {
+				if (scope.cache[i].expires < t) {
+					delete scope.cache[i];
+				}
+			}
+		}, this.ttl/2);
+	};
+	caching.prototype.set = function(label, value) {
+		//Gamify.log("set()", [label, value]);
+		var expires = new Date().getTime()+this.ttl;
+		this.cache[label] = {
+			data:		value,
+			expires:	expires
+		}
+		return expires;
+	}
+	caching.prototype.get = function(label) {
+		if (this.cache[label]) {
+			return this.cache[label].data;
+		}
+		return null;
+	};
+	caching.prototype.clear = function(label) {
+		//Gamify.log("clear",label);
+		delete this.cache[label];
+	};
+
+	this.cache = new caching(1000*20);	// 20sec session caching
+
+}
+socialLoginClass.prototype.setup = function(type, settings) {
+	var scope = this;
+	var passportSetup = {
+		clientID: 			settings.settings.clientID,
+		clientSecret: 		settings.settings.clientSecret,
+		callbackURL: 		this.url+settings.url.callback,
+		passReqToCallback: 	true
+	};
+	if (this.specialCases[type] && this.specialCases[type].setup) {
+		passportSetup = {...passportSetup,...this.specialCases[type].setup}
+	}
+	passport.use(new (this.map[type])(passportSetup, function (req, accessToken, refreshToken,  params,profile, done) {
+		scope.onAuth(req, type, accessToken, refreshToken,  params,profile, done);
+	}));
+	this.app.get(settings.url.auth, passport.authenticate(type, settings.settings.authParameters || {}));
+	this.app.get(settings.url.callback, passport.authenticate(type, {
+		successRedirect: 	settings.url.success,
+		failureRedirect: 	settings.url.fail,
+		failureFlash: 		true
+	}));
+}
+module.exports = socialLoginClass;
